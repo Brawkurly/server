577fb85f96aee7f196194b7d91d78613085adf83
sawol <55649302+sawol@users.noreply.github.com>
2022-08-20 17:29:24 +0900

    [setting] 프로젝트 초기 설정

---
+++ moneytoring/src/main/java/com/brawkurly/moneytoring/MoneytoringApplication.java
@@ -0,0 +1,13 @@
+package com.brawkurly.moneytoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytoringApplication.class, args);
+	}
+
+}
---
+++ moneytoring/src/test/java/com/brawkurly/moneytoring/MoneytoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brawkurly.moneytoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
