9768ca694ba634ab222818ed0696ddab2afbc0db
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-08-21 23:45:35 +0900

    [feat] 오늘 하루 인기 상품 리스트 조회 기능 service에 추가 (#19)

--- moneytoring/src/main/java/com/brawkurly/moneytoring/service/ProductService.java
+++ moneytoring/src/main/java/com/brawkurly/moneytoring/service/ProductService.java
@@ -2,12 +2,15 @@ package com.brawkurly.moneytoring.service;
 
 import com.brawkurly.moneytoring.controller.ProductController;
 import com.brawkurly.moneytoring.domain.ChangePrice;
+import com.brawkurly.moneytoring.domain.ConsumerPriceDto;
 import com.brawkurly.moneytoring.domain.Item;
 import com.brawkurly.moneytoring.repository.ChangePriceRepository;
 import com.brawkurly.moneytoring.repository.ItemRepository;
 import lombok.*;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -19,6 +22,9 @@ public class ProductService {
     private final ItemRepository itemRepository;
 
     private final ChangePriceRepository changePriceRepository;
+
+
+    private final RedisTemplate redisTemplate;
 
     public ProductController.ResponseDto getProductData(Long id){
         ProductController.ResponseDto responseDto = new ProductController.ResponseDto();
@@ -48,6 +54,36 @@ public class ProductService {
             responseDto.getChangePrice().add(changePriceDto);
         }
 
+
+        /*실시간 인기 구매 상품 현황
+         * key : consumer_price(테이블 명):status(상태-purchase/reserve):productId(상품아이디)
+         * */
+        consumerPopularity("purchase", responseDto);
+
+
+        /*실시간 인기 예약 상품 현황
+         * key : consumer_price(테이블 명):status(상태-reserve):productId(상품아이디)
+         * */
+        consumerPopularity("reserve", responseDto);
+
         return responseDto;
+    }
+
+
+    public void consumerPopularity(String status, ProductController.ResponseDto responseDto){
+        for(Long i=1L; i<=8L; i++){
+            Item tmpItem = itemRepository.findById(i).get();
+
+            String key = "consumer_price:" + status + ":" + i;
+            Long cnt  = redisTemplate.opsForList().size(key);
+
+            ProductController.ConsumerPopularityDto consumerPopularityDto = new ProductController.ConsumerPopularityDto(tmpItem.getName(), cnt);
+
+            if(status.equals("purchase")) responseDto.getConsumerPopularityPurchase().add(consumerPopularityDto);
+            else responseDto.getConsumerPopularityReserve().add(consumerPopularityDto);
+        }
+
+        if(status.equals("purchase")) Collections.sort(responseDto.getConsumerPopularityPurchase(), (o1, o2) -> (int) (o2.getCnt()-o1.getCnt()));
+        else Collections.sort(responseDto.getConsumerPopularityReserve(), (o1, o2) -> (int) (o2.getCnt()-o1.getCnt()));
     }
 }
