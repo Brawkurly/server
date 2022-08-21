13fffd11734b7d84dd50251f60bc061005dc024c
sawol <55649302+sawol@users.noreply.github.com>
2022-08-21 19:47:35 +0900

    [feat] 예약 API 개발 (#13) (#15)
    * [feat] 레디스 설정 파일 생성 (#13)
    
    * [feat] 소비자 예약 데이터 저장 (#13)

---
+++ moneytoring/src/main/java/com/brawkurly/moneytoring/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.brawkurly.moneytoring.repository;
+
+import com.brawkurly.moneytoring.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findByName(String name);
+}
