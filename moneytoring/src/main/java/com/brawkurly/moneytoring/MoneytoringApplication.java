6001630ddba15b445d7064074b0bbb374ff48900
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-08-23 15:07:47 +0900

    [feat] 1분 간격으로 소비자 예약, 구매 데이터 생성 (#29)

--- moneytoring/src/main/java/com/brawkurly/moneytoring/MoneytoringApplication.java
+++ moneytoring/src/main/java/com/brawkurly/moneytoring/MoneytoringApplication.java
@@ -1,10 +1,6 @@
 package com.brawkurly.moneytoring;
 
-import com.brawkurly.moneytoring.domain.ChangePrice;
-import com.brawkurly.moneytoring.domain.ConsumerPrice;
 import com.brawkurly.moneytoring.domain.Item;
-import com.brawkurly.moneytoring.repository.ChangePriceRepository;
-import com.brawkurly.moneytoring.repository.ConsumerPriceRepository;
 import com.brawkurly.moneytoring.repository.ItemRepository;
 import com.brawkurly.moneytoring.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-
-import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
 
 
 @EnableScheduling
@@ -37,7 +29,7 @@ public class MoneytoringApplication {
 	// 1/3확률로 예약, 2/3확률로 구매
 	// 가격 예약은 현재판매가보다 작으면서 공급자가-1000보다 크도록
 	// 1분마다 생성
-	@Scheduled(fixedRate = 1000)
+	@Scheduled(fixedRate = 1000*60)
 	public void insertConsumerPrice() {
 		// 상품 1~8 랜덤 값 생성
 		String productId = String.valueOf((int)((Math.random()*10000)%8+1));
